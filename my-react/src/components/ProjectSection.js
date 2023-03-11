d7c28df9e75748f160e236aa4f7921f7ecdc7a43
tws <thdxodns@naver.com>
2023-03-11 16:38:40 +0900

    0311

--- my-react/src/components/ProjectSection.js
+++ my-react/src/components/ProjectSection.js
@@ -13,8 +13,7 @@ function MidSection() {
             <div><h2 className={`${Styles.title_fontsize} ${Styles.font_color}`}>PROJECT</h2></div>
             <div className={Styles.mid_section_container01}>
                 <div className={Styles.mid_cont_box01}>
-                    Web Programming
-                    React UI/UX web site
+                    Web Programming React UI/UX web site
                 </div>
                 <div className={Styles.mid_cont_box02}>
                     WEB PAGE RENEWAL
